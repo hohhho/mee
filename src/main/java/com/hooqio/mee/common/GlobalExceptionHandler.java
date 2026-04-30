3d6cda16f9dd9c4c165bced15b224b558a67d060
hooqio <andysh0701@gmail.com>
2026-04-30 17:09:35 +0900

    feat: add common ApiResponse wrapper and global exception handler
    공통 ApiResponse 래퍼 및 전역 예외 처리기 추가

---
+++ src/main/java/com/hooqio/mee/common/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.hooqio.mee.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        return ResponseEntity
+                .internalServerError()
+                .body(ApiResponse.error(e.getMessage()));
+    }
+}
