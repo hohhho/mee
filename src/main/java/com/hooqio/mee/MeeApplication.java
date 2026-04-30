6732520f5aaf0c46d908e0fe2f23802caed2775f
hooqio <andysh0701@gmail.com>
2026-04-30 14:48:44 +0900

    Initial commit

---
+++ src/main/java/com/hooqio/mee/MeeApplication.java
@@ -0,0 +1,13 @@
+package com.hooqio.mee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hooqio/mee/MeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooqio.mee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
